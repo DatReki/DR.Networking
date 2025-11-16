48f20982985213c9ba39e6410630f96fbc87637e
DatReki <TimoHalofan@outlook.com>
2025-11-16 11:07:48 +0100

    Made a beginning for generating user inforamtion.

---
+++ Api/Models/User.cs
@@ -0,0 +1,34 @@
+﻿namespace Api.Models
+{
+    public class User
+    {
+        public enum GenderType
+        {
+            Male,
+            Female,
+            Other,
+        }
+
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public Personal PersonalInformation { get; set; } = new Personal();
+
+        public class Personal
+        {
+            public string Firstname { get; set; } = string.Empty;
+            public string Surname { get; set; } = string.Empty;
+            public string Lastname { get; set; } = string.Empty;
+            public GenderType Gender { get; set; }
+            public string Country { get; set; } = string.Empty;
+            public string CountryCode { get; set; } = string.Empty;
+            public string Province { get; set; } = string.Empty;
+            public string City { get; set; } = string.Empty;
+            public string Street { get; set; } = string.Empty;
+            public int HouseNumber { get; set; }
+            public string HouseNumberAddition { get; set; } = string.Empty;
+            public string PostalCode { get; set; } = string.Empty;
+        }
+    }
+}
---
+++ Backend/Generate.cs
@@ -0,0 +1,17 @@
+﻿using Api.Models;
+using Bogus;
+
+namespace Backend
+{
+    public class Generate
+    {
+        public static User User()
+        {
+            Faker<User> user = new Faker<User>()
+                .RuleFor(x => x.Username, (y, z) => y.Internet.UserName(z.PersonalInformation.Firstname, z.PersonalInformation.Lastname))
+                .RuleFor(x => x.PersonalInformation.Gender, y => y.PickRandom<User.GenderType>());
+
+            return user.Generate();
+        }
+    }
+}
